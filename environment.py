e1c3e8f31fddcf4bbe79de8bb2b2127fe68c3475
George Kour <gkour@ibm.com>
2020-06-08 23:38:31 +0300

    Allow changing the cue values. return loss when training simple brain.

--- environment.py
+++ environment.py
@@ -8,12 +8,14 @@ class PlusMaze:
         # relevant_cue: 0 is odor, 1 is light
         # correct_value: 1/-1 the identity of the correct cue
 
-        self._state = PlusMaze.random_state()
+        self._state = None
         self._relevant_cue = relevant_cue
-        self._correct_value = correct_value
+        self._correct_cue_value = correct_value
+        self._odor_options = [0,1]
+        self._light_options = [0,1]
 
     def reset(self):
-        self._state = PlusMaze.random_state()
+        self._state = self.random_state()
         return self._state
 
     def step(self, action):
@@ -22,7 +24,7 @@ class PlusMaze:
         selected_cues = [self._state[selected_arm_cues_ind], self._state[selected_arm_cues_ind + 1]]
         #        print('state {}, action:{} selected arm cues:{}'.format(self._state,action,selected_cues ))
         self._state = np.asarray([-1, -1, -1, -1, -1, -1, -1, -1])
-        if selected_cues[self._relevant_cue.value] == self._correct_value:
+        if selected_cues[self._relevant_cue.value] == self._correct_cue_value:
             outcome = config.RewardType.WATER if action in [0, 1] else config.RewardType.FOOD
             return self._state, outcome, 1, None
         return self._state, config.RewardType.NONE, 1, None
@@ -50,13 +52,27 @@ class PlusMaze:
     def state_shape():
         return 8
 
-    @staticmethod
-    def random_state():
-        arm1O = random.choice([1, 2])
-        arm1L = random.choice([1, 2])
+    def set_odor_options(self, options):
+        self._odor_options = options
 
-        # use the same combination for arms 3 and arms 4. select rnadomly.
-        arm3O = random.choice([1, 2])
-        arm3L = random.choice([1, 2])
+    def set_light_options(self, options):
+        self._light_options = options
 
-        return np.asarray([arm1O, arm1L, 3-arm1O, 3-arm1L, arm3O, arm3L, 3-arm3O, 3-arm3L])
+    def set_relevant_cue(self,relevant_cue):
+        self._relevant_cue=relevant_cue
+
+    def set_correct_cue_value(self,correct_cue_value):
+        self._correct_cue_value=correct_cue_value
+
+    def random_state(self):
+        arm1O = random.choice([0, 1])
+        arm1L = random.choice([0, 1])
+
+        # use the same combination for arms 3 and arms 4. select randomly.
+        arm3O = random.choice([0, 1])
+        arm3L = random.choice([0, 1])
+
+        return np.asarray([self._odor_options[arm1O], self._light_options[arm1L],
+                           self._odor_options[1-arm1O], self._light_options[1-arm1L],
+                           self._odor_options[arm3O], self._light_options[arm3L],
+                           self._odor_options[1-arm3O], self._light_options[1-arm3L]])
