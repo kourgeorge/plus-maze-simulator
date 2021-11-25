23623f630b0f1b9b3ee5846fe331a8872bb566b8
George Kour <kourgeorge@gmail.com>
2021-11-25 02:01:16 +0200

    fix stages definition.

--- main.py
+++ main.py
@@ -16,9 +16,10 @@ if __name__ == '__main__':
     num_actions = env.num_actions()
     observation_size = env.state_shape()
 
-    repetitions = 5
-
+    repetitions = 25
+    stages = list(range(1, 6))
     days_per_stage = []
+
     for rep in range(repetitions):
         # brain = TableDQNBrain(num_actions=num_actions, reward_discount=0, learning_rate=config.LEARNING_RATE)
         #brain = BrainPG(observation_size + 1, num_actions, reward_discount=0, learning_rate=config.LEARNING_RATE)
@@ -28,14 +29,14 @@ if __name__ == '__main__':
 
         experiment_report_df = PlusMazeExperiment(agent, dashboard=False)
         c = Counter(list(experiment_report_df['Stage']))
-        days_per_stage.append([c[i] for i in range(1,5)])
+        days_per_stage.append([c[i] for i in stages])
 
     days_per_stage = np.stack(days_per_stage)
 
-    stages = range(1, 6)
+
     width = 0.25
-    fig, ax = plt.subplots(figsize=(20, 12))
-    ax.bar(stages, np.mean(days_per_stage, axis=0), yerr=np.std(days_per_stage, axis=0), color='b', width=width, label='Empirical', capsize=2)
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.bar(stages, np.mean(days_per_stage, axis=0), yerr=np.std(days_per_stage, axis=0), color='b', width=width, label='Num days in stage', capsize=2)
     plt.title("Days Per stage in brain {}: #param={}. #reps={}".format(str(brain),brain.num_trainable_parameters(), repetitions))
 
     plt.show()
