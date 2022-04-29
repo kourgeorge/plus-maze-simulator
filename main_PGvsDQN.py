c0b9ba331b6fed83e0f4bb61257fe9050d90e0f7
George Kour <kourgeorge@gmail.com>
2022-04-29 23:12:53 +0300

    fix experiment.

--- main_PGvsDQN.py
+++ main_PGvsDQN.py
@@ -16,27 +16,32 @@ if __name__ == '__main__':
     num_actions = env.num_actions()
     observation_size = env.state_shape()
 
-    repetitions = 2
+    repetitions = 15
 
     # agents_DQN_spec = [BrainDQNFixedDoorAttention, StandardBrainNetworkAttention, config.MOTIVATED_REWARD, config.NON_MOTIVATED_REWARD]
     # agents_PG_spec = [BrainPGFixedDoorAttention, StandardBrainNetworkAttention, config.MOTIVATED_REWARD, config.NON_MOTIVATED_REWARD]
 
-    agents_DQN_spec = [BrainDQNSeparateNetworks, SeparateNetworkAttention, config.MOTIVATED_REWARD, config.MOTIVATED_REWARD]
-    agents_PG_spec = [BrainPGSeparateNetworks, SeparateNetworkAttention, config.MOTIVATED_REWARD, config.MOTIVATED_REWARD]
+    agents_DQN_spec = [BrainDQNSeparateNetworks, SeparateNetworkAttention, config.MOTIVATED_REWARD, config.NON_MOTIVATED_REWARD]
+    agents_PG_spec = [BrainPGSeparateNetworks, SeparateNetworkAttention, config.MOTIVATED_REWARD, config.NON_MOTIVATED_REWARD]
 
     brains_reports = []
 
     for agent_spec in [agents_DQN_spec, agents_PG_spec]:
         completed_experiments = 0
+        aborted_experiments = 0
         brain_repetition_reports = [None] * repetitions
         while completed_experiments < repetitions:
             agent = MotivatedAgent(agent_spec[0](agent_spec[1](2, 4)), motivation=config.RewardType.WATER,
                                    motivated_reward_value=agent_spec[2], non_motivated_reward_value=agent_spec[3])
-            success, experiment_report_df = PlusMazeExperiment(agent, dashboard=True)
+            success, experiment_report_df = PlusMazeExperiment(agent, dashboard=False)
             if success == EperimentStatus.COMPLETED:
                 brain_repetition_reports[completed_experiments] = experiment_report_df
                 completed_experiments += 1
+            else:
+                aborted_experiments += 1
         brains_reports.append(brain_repetition_reports)
+        print("{} out of {} experiments were aborted".format(aborted_experiments,
+                                                             aborted_experiments + completed_experiments))
 
     plot_behavior_results(brains_reports[0])
     plot_behavior_results(brains_reports[1])
