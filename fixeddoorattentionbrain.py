e65d407a404d5466ef8cd18e202c23f608584f7e
George Kour <kourgeorge@gmail.com>
2022-08-04 16:52:59 +0300

    adapt FixedDoorAttentionBrain to recent refactor

--- fixeddoorattentionbrain.py
+++ fixeddoorattentionbrain.py
@@ -11,10 +11,10 @@ class FixedDoorAttentionBrain(MotivationDependantBrain):
 		super().__init__(*args, **kwargs)
 
 	def think(self, obs, agent: MotivatedAgent):
-		if agent.get_motivation() == config.RewardType.WATER:
-			attention_vec = [1, 1, 0, 0]
+		if agent.get_motivation() == RewardType.WATER:
+			attention_vec = [3, 3, -3, -3]
 		else:
-			attention_vec = [0, 0, 1, 1]
-		action_probs = self.network(torch.FloatTensor(obs), attention_vec)
+			attention_vec = [-3, -3, 3, 3]
+		action_probs = self.network()(torch.FloatTensor(obs), attention_vec)
 
 		return action_probs
