3a6a4ebabd7afaba5d71cd1340191a9a20444c9a
George Kour <kourgeorge@gmail.com>
2022-08-28 22:58:02 +0300

    reverting food and water.

--- fixeddoorattentionbrain.py
+++ fixeddoorattentionbrain.py
@@ -12,9 +12,9 @@ class FixedDoorAttentionBrain(MotivationDependantBrain):
 
 	def think(self, obs, agent: MotivatedAgent):
 		if agent.get_motivation() == RewardType.WATER:
-			attention_vec = [3, 3, -3, -3]
+			attention_vec = [0, 0, 1, 1]
 		else:
-			attention_vec = [-3, -3, 3, 3]
+			attention_vec = [1, 1, 0, 0]
 		action_probs = self.network()(torch.FloatTensor(obs), attention_vec)
 
 		return action_probs
