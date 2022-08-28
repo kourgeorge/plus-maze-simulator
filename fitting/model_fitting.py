77031227bc9ef340c272fc23fef7d44c092b59ea
George Kour <kourgeorge@gmail.com>
2022-08-28 23:04:01 +0300

    fit the nmr lr and batch size parameters.

--- fitting/model_fitting.py
+++ fitting/model_fitting.py
@@ -1,48 +1,57 @@
+import os
+import random
+
 import pandas as pd
 import numpy as np
+import torch
 
 from fitting.PlusMazeExperimentFitting import PlusMazeExperimentFitting
+from fitting.fitting_config import ANIMAL_BATCHES, ANIMAL_DATA_PATH
 from standardbrainnetwork import FullyConnectedNetwork, EfficientNetwork, SeparateMotivationAreasNetwork, \
 	FullyConnectedNetwork2Layers
 from learner import DQN, PG
-from scipy.special import logsumexp
 from scipy.optimize import minimize
 from consolidationbrain import ConsolidationBrain
 import config
-from PlusMazeExperiment import PlusMazeExperiment, ExperimentStatus
 from motivatedagent import MotivatedAgent
 from environment import PlusMazeOneHotCues,CueType
 from rewardtype import RewardType
 
 
 def llik_td(x, *args):
-	results = []
-	repetitions = 3
-	for i in range(repetitions):
-		env = PlusMazeOneHotCues(relevant_cue=CueType.ODOR)
-		[brain, learner, network, motivated_reward_value, non_motivated_reward_value] = \
-			[ConsolidationBrain, DQN, FullyConnectedNetwork, config.MOTIVATED_REWARD, config.NON_MOTIVATED_REWARD]
+	(nmr, lr, batch_size) = x
+	rat_data_file = args[0]
+	rat_data = pd.read_csv(rat_data_file)
 
-		non_motivated_reward_value = x[0]
-		print("Current VARIABLE:{}".format(non_motivated_reward_value))
-		agent = MotivatedAgent(brain(
-			learner(network(env.stimuli_encoding_size(), 2, env.num_actions()), learning_rate=config.LEARNING_RATE)),
-							   motivation=RewardType.WATER,
-							   motivated_reward_value=motivated_reward_value,
-							   non_motivated_reward_value=non_motivated_reward_value)
+	torch.manual_seed(0)
+	np.random.seed(0)
+	random.seed(0)
 
-		_, all_experiment_likelihoods = PlusMazeExperimentFitting(agent, dashboard=False, rat_data_file=rat_data_file)
-		results += [np.median(all_experiment_likelihoods)]
+	env = PlusMazeOneHotCues(relevant_cue=CueType.ODOR)
+	[brain, learner, network, motivated_reward_value, non_motivated_reward_value] = \
+		[ConsolidationBrain, DQN, FullyConnectedNetwork2Layers, config.MOTIVATED_REWARD, config.NON_MOTIVATED_REWARD]
 
-	return np.mean(results)
+	print("Current VARIABLE:{}".format(x))
+	agent = MotivatedAgent(brain(
+		learner(network(env.stimuli_encoding_size(), 2, env.num_actions()), learning_rate=lr), batch_size),
+		motivation=RewardType.WATER,
+		motivated_reward_value=motivated_reward_value,
+		non_motivated_reward_value=nmr)
+
+	_, all_experiment_likelihoods = PlusMazeExperimentFitting(agent, dashboard=False, rat_data=rat_data)
+	return np.median(all_experiment_likelihoods)
+
 
 
 if __name__ == '__main__':
 	# load behavioural data
-	expr_data = {1: [1, 2], 2: [1], 4: [6, 7, 8], 5: [1, 2], 6: [10, 11]}
-	rat_data_file = './output_expr{}_rat{}.csv'.format(1, 1)
-	num_trials = len((pd.read_csv(rat_data_file)).index)
-	x0 = 0.3
-	bnds = ((0, 0.5),)
-	result = minimize(llik_td, x0, bounds=bnds)
-	print(result)
+	nmr = 0.5
+	lr = 0.005
+	batch_size = 2
+	bnds = ((0, 0.7),(0.001, 0.01), (1,20))
+	for animal_batch in ANIMAL_BATCHES:
+		for rat in ANIMAL_BATCHES[animal_batch]:
+			rat_data_path = os.path.join(ANIMAL_DATA_PATH, 'output_expr{}_rat{}.csv'.format(animal_batch, rat))
+			rat_id = '{}_{}'.format(animal_batch, rat)
+			result = minimize(llik_td, [nmr, lr, batch_size], rat_data_path, bounds=bnds)
+			print(result)
