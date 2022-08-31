a467b02bcfbd48b7c79d8686c7917103815a5fc4
George Kour <kourgeorge@gmail.com>
2022-09-01 02:24:34 +0300

    Baysian optimization to find best hyperparameter combination.

--- fitting/fitting_config.py
+++ fitting/fitting_config.py
@@ -13,15 +13,16 @@ REPETITIONS = 3
 ANIMAL_DATA_PATH = './behavioral_data'
 ANIMAL_BATCHES = {1: [1, 2], 2: [1], 4: [6, 7, 8], 5: [1, 2], 6: [10, 11]}
 
-learning_rates = [0.001, 0.005, 0.01]
-brains = [#(ConsolidationBrain, DQN, FullyConnectedNetwork),
+non_motivated_reward = [0, 0.3, 0.7]
+learning_rates = [0.001, 0.005, 0.01, 0.2]
+brains = [(ConsolidationBrain, DQN, FullyConnectedNetwork),
 		  (ConsolidationBrain, DQN, FullyConnectedNetwork2Layers),
 		  (ConsolidationBrain, DQN, EfficientNetwork),
-		  #(FixedDoorAttentionBrain, DQN, EfficientNetwork),
+		  (FixedDoorAttentionBrain, DQN, EfficientNetwork),
 		  (MotivationDependantBrain, DQN, SeparateMotivationAreasNetwork),
 		  (LateOutcomeEvaluationBrain, DQN, SeparateMotivationAreasNetwork),
-		  (RandomBrain, DQN, EfficientNetwork)]
-non_motivated_reward = [0.3]
+		  (RandomBrain, DQN, EfficientNetwork)
+		  ]
 
 combinations = list(itertools.product(brains, learning_rates, non_motivated_reward))
 configs = [{'br': br, 'lr': lr, 'non_motivated_reward': nmr} for br, lr, nmr in combinations]
--- fitting/model_fitting.py
+++ fitting/model_fitting.py
@@ -1,12 +1,17 @@
 import os
+import pickle
 import random
+import sys
 
 import pandas as pd
 import numpy as np
-import torch
+from matplotlib import pyplot as plt
+from skopt import gp_minimize
+from skopt import plots
+from skopt.space import Real, Integer
 
 from fitting.PlusMazeExperimentFitting import PlusMazeExperimentFitting
-from fitting.fitting_config import ANIMAL_BATCHES, ANIMAL_DATA_PATH
+from fitting.fitting_config import ANIMAL_BATCHES, ANIMAL_DATA_PATH, brains
 from standardbrainnetwork import FullyConnectedNetwork, EfficientNetwork, SeparateMotivationAreasNetwork, \
 	FullyConnectedNetwork2Layers
 from learner import DQN, PG
@@ -14,44 +19,139 @@ from scipy.optimize import minimize
 from consolidationbrain import ConsolidationBrain
 import config
 from motivatedagent import MotivatedAgent
-from environment import PlusMazeOneHotCues,CueType
+from environment import PlusMazeOneHotCues, CueType
 from rewardtype import RewardType
 
+import seaborn as sns
 
-def llik_td(x, *args):
+animal_batch = None
+rat = None
+architecture = None
+
+results_file_name = 'results_mean_stage_likelihood.pkl'
+
+env = PlusMazeOneHotCues(relevant_cue=CueType.ODOR)
+
+# Disable
+def blockPrint():
+	sys.stdout = open(os.devnull, 'w')
+
+# Restore
+def enablePrint():
+	sys.stdout = sys.__stdout__
+
+def llik_td(x):
+	# torch.manual_seed(0)
+	# np.random.seed(0)
+	# random.seed(0)
+
 	(nmr, lr, batch_size) = x
-	rat_data_file = args[0]
-	rat_data = pd.read_csv(rat_data_file)
+	rat_data_path = os.path.join(ANIMAL_DATA_PATH, 'output_expr{}_rat{}.csv'.format(animal_batch, rat))
+	rat_data = pd.read_csv(rat_data_path)
 
-	torch.manual_seed(0)
-	np.random.seed(0)
-	random.seed(0)
-
-	env = PlusMazeOneHotCues(relevant_cue=CueType.ODOR)
-	[brain, learner, network, motivated_reward_value, non_motivated_reward_value] = \
-		[ConsolidationBrain, DQN, FullyConnectedNetwork2Layers, config.MOTIVATED_REWARD, config.NON_MOTIVATED_REWARD]
-
-	print("Current VARIABLE:{}".format(x))
+	brain, learner, network = architecture
 	agent = MotivatedAgent(brain(
-		learner(network(env.stimuli_encoding_size(), 2, env.num_actions()), learning_rate=lr), batch_size),
+		learner(network(env.stimuli_encoding_size(), 2, env.num_actions()), learning_rate=lr), batch_size=batch_size),
 		motivation=RewardType.WATER,
-		motivated_reward_value=motivated_reward_value,
+		motivated_reward_value=config.MOTIVATED_REWARD,
 		non_motivated_reward_value=nmr)
 
-	_, all_experiment_likelihoods = PlusMazeExperimentFitting(agent, dashboard=False, rat_data=rat_data)
-	return np.median(all_experiment_likelihoods)
+	blockPrint()
+	experiment_stats, all_experiment_likelihoods = PlusMazeExperimentFitting(agent, dashboard=False, rat_data=rat_data)
+	enablePrint()
 
+	daily_likelihoods = experiment_stats.epoch_stats_df.Likelihood
+	stages = experiment_stats.epoch_stats_df.Stage
+	likelihood_stage = np.zeros([5])
+	for stage in range(5):
+		stage_likelihood = [daily_likelihoods[i] for i in range(len(daily_likelihoods)) if stages[i] == stage]
+		if len(stage_likelihood) == 0:
+			likelihood_stage[stage] = None
+		else:
+			likelihood_stage[stage] = np.mean(stage_likelihood)
+	return likelihood_stage, all_experiment_likelihoods
+
+
+def fun(x):
+	likelihood_stage, all_experiment_likelihoods = llik_td(x)
+	y = np.nanmean(likelihood_stage)
+	print("x={} ,y={}, stages={}".format(x, y, likelihood_stage))
+	return y
+
+
+def brain_name(architecture):
+	return "{}.{}.{}".format(architecture[0].__name__, architecture[1].__name__, architecture[2].__name__)
+
+
+def run_all_data():
+	global animal_batch, rat, architecture
+	# load behavioural data
+	x0_nmr = 0.3
+	x0_lr = 0.005
+	x0_batch_size = 10
+
+	nmr = Real(name='nmr', low=0.0, high=1)
+	lr = Real(name='lr', low=0.0001, high=0.1, prior='log-uniform')
+	batch_size = Integer(name='batch_size', low=1, high=20)
+	space = [nmr, lr, batch_size]
+
+	results = {}
+	for animal_batch in ANIMAL_BATCHES:
+		for rat in ANIMAL_BATCHES[animal_batch]:
+			animal_id = "{}_{}".format(animal_batch, rat)
+			results[animal_id] = {}
+			for architecture in brains:
+				search_result = gp_minimize(fun, space, x0=(x0_nmr, x0_lr, x0_batch_size), n_calls=30)
+				likelihood_stage, all_experiment_likelihoods = llik_td(search_result.x)
+				results[animal_id][brain_name(architecture)] = {"likelihood": search_result.fun,
+																"values": search_result.x,
+																"stages": likelihood_stage,
+																"results": search_result}
+
+	with open(results_file_name, 'wb') as f:
+		pickle.dump(results, f)
+# for fitness, x in sorted(zip(search_result.func_vals, search_result.x_iters)):
+# 	print(fitness, x)
+
+
+def plot_resuls():
+	with open(results_file_name, 'rb') as f:
+		results = pickle.load(f)
+
+	ds = pd.DataFrame()
+	for rat, rat_value in results.items():
+		for brain, brain_results in rat_value.items():
+			row = {'brain': brain,
+				   'animal': rat,
+				   'likelihood': np.round(1.5-brain_results['likelihood'], 3),
+				   'nmr': np.round(brain_results['values'][0], 3),
+				   'lr': np.round(brain_results['values'][1], 3),
+				   'bs': np.round(brain_results['values'][2], 3)
+				   }
+			ds = ds.append(row, ignore_index=True)
+	# params = "(nmr:{}, lr:{}, bs:{})".format(*np.round(brain_results['values'], 3))
+	# print("{}, {}: \t{} {}".format(rat, brain, np.round(brain_results['likelihood'],3), params))
+	# plots.plot_histogram(result=brain_results, dimension_identifier='lr', bins=20)
+	# plots.plot_objective_2D(brain_results['results'], 'lr', 'batch_size')
+	# plots.plot_objective(brain_results['results'], plot_dims=['nmr', 'lr'])
+
+	sns.set_theme(style="darkgrid")
+
+	#f, axes = plt.subplots(3, 3)
+
+	for i, animal in enumerate(np.unique(ds['animal'])):
+		plt.figure(figsize=(3.5,3.5))
+		ds_filtered = ds[ds['animal'] == animal]
+		g = sns.scatterplot(y="lr", x="nmr",
+						hue="brain", size='likelihood',
+						data=ds_filtered, sizes=(20, 200),  alpha= 0.7)
+
+		#g.set(yscale="log")
+		g.yaxis.grid(True, "minor", linewidth=.25)
+
+	x = 1
 
 
 if __name__ == '__main__':
-	# load behavioural data
-	nmr = 0.5
-	lr = 0.005
-	batch_size = 2
-	bnds = ((0, 0.7),(0.001, 0.01), (1,20))
-	for animal_batch in ANIMAL_BATCHES:
-		for rat in ANIMAL_BATCHES[animal_batch]:
-			rat_data_path = os.path.join(ANIMAL_DATA_PATH, 'output_expr{}_rat{}.csv'.format(animal_batch, rat))
-			rat_id = '{}_{}'.format(animal_batch, rat)
-			result = minimize(llik_td, [nmr, lr, batch_size], rat_data_path, bounds=bnds)
-			print(result)
+	run_all_data()
+	# plot_resuls()
