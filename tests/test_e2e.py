a8307742f70de6f8cc9f3aea16ad766911dfb756
George Kour <kourgeorge@gmail.com>
2022-09-02 03:37:20 +0300

    adding tabular test.

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -3,12 +3,13 @@ __author__ = 'gkour'
 from environment import PlusMazeOneHotCues, CueType
 from rewardtype import RewardType
 from standardbrainnetwork import FullyConnectedNetwork, EfficientNetwork, SeparateMotivationAreasNetwork, \
-    FullyConnectedNetwork2Layers
+    FullyConnectedNetwork2Layers, TabularQ
 from fixeddoorattentionbrain import FixedDoorAttentionBrain
 from learner import DQN
 from consolidationbrain import ConsolidationBrain
 from motivatedagent import MotivatedAgent
 from PlusMazeExperiment import PlusMazeExperiment
+from table_dqn_brain import TD, TableDQNBrain
 
 
 def test_BasicDQNBrain():
@@ -35,3 +36,20 @@ def test_FixedDoorAttentionBrain():
     experiment_stats = PlusMazeExperiment(agent, dashboard=False)
 
     assert experiment_stats is not None
+
+
+def test_tabularBrain():
+    env = PlusMazeOneHotCues(relevant_cue=CueType.ODOR)
+    learner = TD(learning_rate=0.01, model=TabularQ(env.stimuli_encoding_size(), 2, env.num_actions()))
+    brain = TableDQNBrain(learner, env.num_actions())
+
+    agent = MotivatedAgent(brain,motivation=RewardType.WATER,
+                                       motivated_reward_value=1, non_motivated_reward_value=0.3)
+    experiment_stats = PlusMazeExperiment(agent, dashboard=False)
+
+    assert experiment_stats is not None
+
+
+if __name__ == '__main__':
+    #test_FixedDoorAttentionBrain()
+    test_tabularBrain()
