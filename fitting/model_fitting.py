e0af0e76a37de7934ec33a4cd6fe06f852f7f500
George Kour <kourgeorge@gmail.com>
2022-09-04 13:19:58 +0300

    block printing and plot optimization results.

--- fitting/model_fitting.py
+++ fitting/model_fitting.py
@@ -12,17 +12,18 @@ from skopt.space import Real, Integer
 
 from fitting.PlusMazeExperimentFitting import PlusMazeExperimentFitting
 from fitting.fitting_config import ANIMAL_BATCHES, ANIMAL_DATA_PATH, brains
-from standardbrainnetwork import FullyConnectedNetwork, EfficientNetwork, SeparateMotivationAreasNetwork, \
-	FullyConnectedNetwork2Layers
-from learner import DQN, PG
-from scipy.optimize import minimize
-from consolidationbrain import ConsolidationBrain
 import config
+from fitting_utils import get_timestamp
 from motivatedagent import MotivatedAgent
 from environment import PlusMazeOneHotCues, CueType
 from rewardtype import RewardType
 
 import seaborn as sns
+
+import warnings
+warnings.filterwarnings("ignore")
+
+n_calls = 50
 
 animal_batch = None
 rat = None
@@ -58,7 +59,6 @@ def llik_td(x):
 		motivated_reward_value=config.MOTIVATED_REWARD,
 		non_motivated_reward_value=nmr)
 
-	blockPrint()
 	experiment_stats, all_experiment_likelihoods = PlusMazeExperimentFitting(agent, dashboard=False, rat_data=rat_data)
 	enablePrint()
 
@@ -89,12 +89,12 @@ def brain_name(architecture):
 def run_all_data():
 	global animal_batch, rat, architecture
 	# load behavioural data
-	x0_nmr = 0.3
-	x0_lr = 0.005
+	x0_nmr = 0.2
+	x0_lr = 0.15
 	x0_batch_size = 10
 
 	nmr = Real(name='nmr', low=0.0, high=1)
-	lr = Real(name='lr', low=0.0001, high=0.1, prior='log-uniform')
+	lr = Real(name='lr', low=0.0001, high=0.2, prior='log-uniform')
 	batch_size = Integer(name='batch_size', low=1, high=20)
 	space = [nmr, lr, batch_size]
 
@@ -102,9 +102,10 @@ def run_all_data():
 	for animal_batch in ANIMAL_BATCHES:
 		for rat in ANIMAL_BATCHES[animal_batch]:
 			animal_id = "{}_{}".format(animal_batch, rat)
+			print(animal_id)
 			results[animal_id] = {}
 			for architecture in brains:
-				search_result = gp_minimize(fun, space, x0=(x0_nmr, x0_lr, x0_batch_size), n_calls=30)
+				search_result = gp_minimize(fun, space, x0=(x0_nmr, x0_lr, x0_batch_size), n_calls=n_calls)
 				likelihood_stage, all_experiment_likelihoods = llik_td(search_result.x)
 				results[animal_id][brain_name(architecture)] = {"likelihood": search_result.fun,
 																"values": search_result.x,
@@ -113,12 +114,14 @@ def run_all_data():
 
 	with open(results_file_name, 'wb') as f:
 		pickle.dump(results, f)
+
+
 # for fitness, x in sorted(zip(search_result.func_vals, search_result.x_iters)):
 # 	print(fitness, x)
 
 
-def plot_resuls():
-	with open(results_file_name, 'rb') as f:
+def plot_results(results_file_path):
+	with open(results_file_path, 'rb') as f:
 		results = pickle.load(f)
 
 	ds = pd.DataFrame()
@@ -126,12 +129,16 @@ def plot_resuls():
 		for brain, brain_results in rat_value.items():
 			row = {'brain': brain,
 				   'animal': rat,
-				   'likelihood': np.round(1.5-brain_results['likelihood'], 3),
+				   'stages': np.round(brain_results['stages'], 3),
+				   'likelihood': np.round(brain_results['likelihood'], 3),
 				   'nmr': np.round(brain_results['values'][0], 3),
 				   'lr': np.round(brain_results['values'][1], 3),
 				   'bs': np.round(brain_results['values'][2], 3)
 				   }
 			ds = ds.append(row, ignore_index=True)
+			for stage in range(5):
+				ds['stage{}'.format(stage + 1)] = np.stack(list(ds['stages']))[:, stage]
+
 	# params = "(nmr:{}, lr:{}, bs:{})".format(*np.round(brain_results['values'], 3))
 	# print("{}, {}: \t{} {}".format(rat, brain, np.round(brain_results['likelihood'],3), params))
 	# plots.plot_histogram(result=brain_results, dimension_identifier='lr', bins=20)
@@ -140,21 +147,25 @@ def plot_resuls():
 
 	sns.set_theme(style="darkgrid")
 
-	#f, axes = plt.subplots(3, 3)
+	df_unpivot = pd.melt(ds, id_vars=['brain', 'animal'], value_vars=['stage1', 'stage2', 'stage3', 'stage4', 'stage5'])
+	df_unpivot['likelihood'] = df_unpivot['value']
+	sns.boxplot(x='variable', y='likelihood', hue='brain', data=df_unpivot)
+	del ds['stages']
+	# f, axes = plt.subplots(3, 3)
 
 	for i, animal in enumerate(np.unique(ds['animal'])):
-		plt.figure(figsize=(3.5,3.5))
+		plt.figure(figsize=(3.5, 3.5))
 		ds_filtered = ds[ds['animal'] == animal]
 		g = sns.scatterplot(y="lr", x="nmr",
-						hue="brain", size='likelihood',
-						data=ds_filtered, sizes=(20, 200),  alpha= 0.7)
+							hue="brain", size='likelihood',
+							data=ds_filtered, sizes=(20, 200), alpha=0.7)
 
-		#g.set(yscale="log")
+		# g.set(yscale="log")
 		g.yaxis.grid(True, "minor", linewidth=.25)
 
 	x = 1
 
 
 if __name__ == '__main__':
-	run_all_data()
-	# plot_resuls()
+	#run_all_data()
+	plot_results('fitting/fitting_results_2022_09_03_03_43.pkl')
