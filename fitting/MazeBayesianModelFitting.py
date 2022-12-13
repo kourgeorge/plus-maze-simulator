d2df2b8432c6c6438b9b8a734570804517538cdf
George Kour <kourgeorge@gmail.com>
2022-12-13 08:48:20 +0200

    Improve results printing.

--- fitting/MazeBayesianModelFitting.py
+++ fitting/MazeBayesianModelFitting.py
@@ -6,6 +6,7 @@ import warnings
 import numpy as np
 import pandas as pd
 from skopt import gp_minimize
+import scipy
 
 import config
 from environment import PlusMazeOneHotCues2ActiveDoors, CueType
@@ -22,6 +23,8 @@ warnings.filterwarnings("ignore")
 
 
 class MazeBayesianModelFitting:
+	LvsNLL = []
+
 	def __init__(self, env, experiment_data, model, parameters_space, n_calls):
 		self.env = env
 		self.experiment_data = experiment_data
@@ -68,18 +71,36 @@ class MazeBayesianModelFitting:
 		geomeanL = scipy.stats.mstats.gmean(rat_data_with_likelihood.likelihood, nan_policy='omit')
 		np.testing.assert_almost_equal(np.exp(-meanNLL), geomeanL)
 
-		y = np.nansum(NLL)
+		y = meanNLL
 
-		print("{}.\tx={},\t(meanNLL={:.3f}, medianNLL={:.3f}, sumNLL={:.3f}), \t(meanL={:.3f}, medianL={:.3f}), \tgmeanL={:.3f}".format(fitting_utils.brain_name(model),
-																				list(np.round(parameters, 4)),
-																				y,
-																				np.round(items, 3),
-																				np.nanmedian(items)))
-		return np.clip(y, a_min=-50, a_max=50)
+		print("{}.\tx={},\t(meanNLL={:.3f}, medianNLL={:.3f}, sumNLL={:.3f}, stages={}), \t(meanL={:.3f}, "
+			  "medianL={:.3f}, gmeanL={:.3f}, stages={})".format(fitting_utils.brain_name(model),
+																 list(np.round(parameters, 4)),
+																 meanNLL, np.nanmedian(NLL), np.nansum(NLL),
+																 np.round(likelihood_stage.NLL.to_numpy(), 2),
+																 meanL, np.nanmedian(L),
+																 geomeanL,
+																 np.round(likelihood_stage.likelihood.to_numpy(), 2)))
+
+
+
+		# if meanL > 0.68 or meanNLL < 0.61:
+		# 	fig = plt.figure(dpi=120, facecolor='w')
+		# 	axis = fig.add_subplot(2, 1, 1)
+		# 	rat_data_with_likelihood.likelihood.hist(ax=axis)
+		# 	axis.set_title('{}:{}, L,NLL={},{}'.format(model[1], np.round(parameters, 3), np.round(meanL,2), np.round(meanNLL,2)))
+		# 	axis.set_ylabel('likelihood')
+		# 	axis = fig.add_subplot(2, 1, 2)
+		# 	rat_data_with_likelihood.NLL.hist(ax=axis)
+		# 	axis.set_ylabel('NLL')
+
+		MazeBayesianModelFitting.LvsNLL+=[[meanL, meanNLL]]
+
+		return np.clip(y, a_min=-5000, a_max=5000)
 
 	def optimize(self):
 		search_result = gp_minimize(self._calc_experiment_likelihood, self.parameters_space, n_calls=self.n_calls)
-		print("Best Parameters: {}".format(search_result.x))
+		print("Best Parameters: {}".format(np.round(search_result.x, 4)))
 		experiment_stats, rat_data_with_likelihood = self._run_model(search_result.x)
 		return search_result, experiment_stats, rat_data_with_likelihood
 
@@ -88,8 +109,12 @@ class MazeBayesianModelFitting:
 			pass
 
 		print("Warning!! You are running Fake Optimization. This should be used for development purposes only!!")
+		(brain, learner, model) = self.model
+		if issubclass(learner, MALearner) or issubclass(learner, DQNAtt):
+			x = (1.5, 0.05, 15)
+		else:
+			x= (1.5, 0.05)
 
-		x = (1.5, 0.05, 15)
 		obj = Object()
 		obj.x = x
 		experiment_stats, rat_data_with_likelihood = self._run_model(x)
@@ -100,7 +125,7 @@ class MazeBayesianModelFitting:
 		animal_data = [pd.read_csv(os.path.join(animals_data_folder, rat_file))
 					   for rat_file in list(np.sort(os.listdir(animals_data_folder)))]
 
-		timestamp=fitting_utils.get_timestamp()
+		timestamp = fitting_utils.get_timestamp()
 		fitting_results = {}
 		results_df = pd.DataFrame()
 		for subject_id, curr_rat in enumerate(animal_data):
@@ -110,6 +135,12 @@ class MazeBayesianModelFitting:
 				model, parameters_space = curr_model
 				search_result, experiment_stats, rat_data_with_likelihood = \
 					MazeBayesianModelFitting(env, curr_rat, model, parameters_space, n_calls).optimize()
+				#plot correlations
+				# plt.scatter(np.array(MazeBayesianModelFitting.LvsNLL)[:, 0], np.array(MazeBayesianModelFitting.LvsNLL)[:, 1])
+				# plt.xlabel('L')
+				# plt.ylabel('NLL')
+				# plt.title(str(model[1]))
+
 				rat_data_with_likelihood['subject'] = subject_id
 				rat_data_with_likelihood["model"] = fitting_utils.brain_name(model)
 				rat_data_with_likelihood["parameters"] = [search_result.x] * len(rat_data_with_likelihood)
