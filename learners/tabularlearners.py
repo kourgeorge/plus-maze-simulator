e050ced1c597b48fef06bb0d69dec35f7d9b6ba2
George Kour <kourgeorge@gmail.com>
2023-01-07 01:04:11 +0200

    fix the attention update rule for coth SAARL and AARL.

--- learners/tabularlearners.py
+++ learners/tabularlearners.py
@@ -116,13 +116,9 @@ class MALearner(IALearner):
 		self.alpha_phi = alpha_phi
 
 	def learn(self, state_batch, action_batch, reward_batch, action_values, nextstate_batch):
-		oldV = copy.deepcopy(self.model.V)
 
 		super().learn(state_batch, action_batch, reward_batch, action_values, nextstate_batch)
-		# learning_rate = self.optimizer['learning_rate']
-		# delta_value = [1/learning_rate*np.linalg.norm(self.model.V[dimension]-oldV[dimension], ord=1) for dimension in oldV.keys()]
 
-		learning_rate = self.optimizer['learning_rate']
 		actions = np.argmax(action_batch, axis=1)
 
 		all_action_values = self.model(state_batch)
@@ -137,31 +133,18 @@ class MALearner(IALearner):
 			V = np.array([self.model.V['odors'][selected_odor],
 						  self.model.V['colors'][selected_color],
 						  self.model.V['spatial'][selected_door]])
-			delta_phi = self.calc_delta_phi(delta, reward, V, phi_s)
+			delta_phi = self.calc_delta_phi(selected_action_value, reward, V)
 
 			self.model.phi += self.alpha_phi * delta * phi_s * delta_phi
 
 		return deltas
 
-	def calc_delta_phi(self, delta_v, reward, V, phi_s):
-		"""
-		:param delta_v: r-Q where Q is the total value
-		:param reward: r
-		:param V: the feature value
-		:param phi_s: the normalized attention
-		:return: delta_phi
-		"""
-		# return np.array([2 * delta_v * np.matmul(V, [1 - phi_s[0], -phi_s[1], -phi_s[2]]),
-		# 				 2 * delta_v * np.matmul(V, [-phi_s[0], 1 - phi_s[1], -phi_s[2]]),
-		# 				 2 * delta_v * np.matmul(V, [-phi_s[0], -phi_s[1], 1 - phi_s[2]])])
-		# #
-		return np.array([np.matmul(V, [1 - phi_s[0], -phi_s[1], -phi_s[2]]),
-								 np.matmul(V, [-phi_s[0], 1 - phi_s[1], -phi_s[2]]),
-								 np.matmul(V, [-phi_s[0], -phi_s[1], 1 - phi_s[2]])])
+	def calc_delta_phi(self, Q, reward, V):
+		return V-Q
 
 
 class MALearnerSimple(MALearner):
-	def calc_delta_phi(self, delta_v, reward, V, phi_s):
-		return -delta_v * (reward - V)
+	def calc_delta_phi(self, Q, reward, V):
+		return V-reward
 
 
