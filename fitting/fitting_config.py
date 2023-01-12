59ca6f59e3b6ddf2211bf18ff346794afdcdce57
George Kour <kourgeorge@gmail.com>
2023-01-13 01:32:38 +0200

    Switching back to Bayesian optimization.

--- fitting/fitting_config.py
+++ fitting/fitting_config.py
@@ -18,7 +18,7 @@ from models.tabularmodels import PCFTable
 REPETITIONS = 3
 MOTIVATED_ANIMAL_DATA_PATH = './fitting/motivation_behavioral_data'
 MAZE_ANIMAL_DATA_PATH = './fitting/maze_behavioral_data'
-BAYESIAN_OPTIMIZATION = False
+BAYESIAN_OPTIMIZATION = True
 MOTIVATED_ANIMAL_BATCHES = {1: [1, 2], 2: [1], 4: [6, 7, 8], 5: [1, 2], 6: [10, 11]}
 MAZE_ANIMALS = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
@@ -26,8 +26,8 @@ nmr = Real(name='nmr', low=-1, high=1)
 batch_size = Integer(name='batch_size', low=1, high=20)
 
 if BAYESIAN_OPTIMIZATION:
-	lr = Real(name='lr', low=0.001, high=0.2, prior='log-uniform')
-	attention_lr = Real(name='attention_lr', low=0.001, high=0.2, prior='log-uniform')
+	lr = Real(name='lr', low=0.001, high=0.4, prior='log-uniform')
+	attention_lr = Real(name='attention_lr', low=0.001, high=0.4, prior='log-uniform')
 	beta = Real(name='beta', low=0.1, high=30, prior='log-uniform')
 	initial_value = Real(name='initial', low=0, high=0.1, prior='uniform')
 else:
@@ -37,17 +37,21 @@ else:
 	initial_value = (0, 0.15)
 
 maze_models = [((TDBrain, QLearner, QTable), (beta, lr)),
-			   # ((TDBrain, OptionsLearner, OptionsTable), (beta, lr)),
+			   ((TDBrain, OptionsLearner, OptionsTable), (beta, lr)),
 			   ((TDBrain, IALearner, ACFTable), (beta, lr)),
-			   # ((TDBrain, IAAluisiLearner, ACFTable), (beta, lr)),
+			   ((TDBrain, IAAluisiLearner, ACFTable), (beta, lr)),
 			   ((TDBrain, MALearner, ACFTable), (beta, lr, attention_lr)),
+
 			   # ((TDBrain, MALearnerSimple, ACFTable), (beta, lr, attention_lr)),
 			   # ((TDBrain, MALearner, PCFTable), (beta, lr, attention_lr)),
+
 			   ((ConsolidationBrain, DQN, FCNet), (beta, lr)),
 			   ((ConsolidationBrain, DQN, UANet), (beta, lr)),
 			   ((ConsolidationBrain, DQN, ACLNet), (beta, lr)),
 			   ((ConsolidationBrain, DQNAtt, ACLNet), (beta, lr, attention_lr)),
-			   # ((ConsolidationBrain, DQN, FC2LayersNet), (beta, lr)),
+			   ((ConsolidationBrain, DQN, FC2LayersNet), (beta, lr)),
+
+				#((ConsolidationBrain, DQN, EfficientNetwork),(beta, lr)),
 				# ((ConsolidationBrain, PG, FullyConnectedNetwork), (beta, lr)),
 				# ((ConsolidationBrain, PG, UniformAttentionNetwork), (beta, lr)),
 				# ((ConsolidationBrain, PG, AttentionAtChoiceAndLearningNetwork), (beta, lr)),
